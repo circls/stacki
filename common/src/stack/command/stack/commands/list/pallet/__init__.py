a8f311188fc5caf6b9832d96b4ea140ec04591e8
knutsonchris <knutsoonchris@gmail.com>
2018-06-11 14:36:16 -0700

    FEATURE: add parameter 'display-url=true' to stack list pallet to optionally display URL information about each pallet

--- common/src/stack/command/stack/commands/list/pallet/__init__.py
+++ common/src/stack/command/stack/commands/list/pallet/__init__.py
@@ -14,58 +14,95 @@ import stack.commands
 
 
 class Command(stack.commands.RollArgumentProcessor,
-	stack.commands.list.command):
-	"""
-	List the status of available pallets.
-	
-	<arg optional='1' type='string' name='pallet' repeat='1'>
-	List of pallets. This should be the pallet base name (e.g., base, hpc,
-	kernel). If no pallets are listed, then status for all the pallets are
-	listed.
-	</arg>
+        stack.commands.list.command):
+        """
+        List the status of available pallets.
 
-	<example cmd='list pallet kernel'>		
-	List the status of the kernel pallet.
-	</example>
-	
-	<example cmd='list pallet'>
-	List the status of all the available pallets.
-	</example>
+        <arg optional='1' type='string' name='pallet' repeat='1'>
+        List of pallets. This should be the pallet base name (e.g., base, hpc,
+        kernel). If no pallets are listed, then status for all the pallets are
+        listed.
+        </arg>
 
-	<related>add pallet</related>
-	<related>remove pallet</related>
-	<related>enable pallet</related>
-	<related>disable pallet</related>
-	<related>create pallet</related>
-	"""		
+        <example cmd='list pallet kernel'>
+        List the status of the kernel pallet.
+        </example>
 
-	def run(self, params, args):
-		self.beginOutput()
+        <example cmd='list pallet'>
+        List the status of all the available pallets.
+        </example>
 
-		for (roll, version, release) in self.getRollNames(args, params):
-			self.db.execute("""select r.arch, r.os from rolls r
-				where r.name='%s' and r.version='%s' and
-				r.rel='%s' """
-				% (roll, version, release))
-			
-			# For each pallet determine it is enabled
-			# in any boxes.
-			
-			for arch, osname in self.db.fetchall():
-				self.db.execute("""select b.name from
-					stacks s, rolls r, boxes b where
-					r.name='%s' and r.arch='%s' and
-					r.version='%s' and r.rel='%s' and
-					s.roll=r.id and s.box=b.id """
-					% (roll, arch, version, release))
+        <related>add pallet</related>
+        <related>remove pallet</related>
+        <related>enable pallet</related>
+        <related>disable pallet</related>
+        <related>create pallet</related>
+        """
 
-				boxes = []
-				for box, in self.db.fetchall():
-					boxes.append(box)
+        def run(self, params, args):
+                self.beginOutput()
 
-				self.addOutput(roll, (version, release, arch,
-						      osname, ' '.join(boxes)))
+                accepted_values = ['True', 'true', 'T', 't', 'Y', 'y']
+                if 'display-url' in params and params['display-url'] in accepted_values:
 
-		self.endOutput(header=['name', 'version', 'release', 'arch',
-			'os', 'boxes'], trimOwner=False)
-		
+                        for (roll, version, release) in self.getRollNames(args, params):
+                                self.db.execute("""select r.arch, r.os, r.URL from rolls r
+                                        where r.name='%s' and r.version='%s' and
+                                        r.rel='%s' """
+                                        % (roll, version, release))
+
+                                # For each pallet determine it is enabled
+                                # in any boxes.
+
+                                for arch, osname, URL in self.db.fetchall():
+                                        self.db.execute("""select b.name from
+                                                stacks s, rolls r, boxes b where
+                                                r.name='%s' and r.arch='%s' and
+                                                r.version='%s' and r.rel='%s' and
+                                                s.roll=r.id and s.box=b.id """
+												% (roll, arch, version, release))
+
+                                        boxes = []
+                                        for box, in self.db.fetchall():
+                                                boxes.append(box)
+
+
+                                        self.addOutput(roll, (version, release, arch,
+                                                              osname, ' '.join(boxes), URL))
+
+                        self.endOutput(header=['name', 'version', 'release', 'arch',
+                                'os', 'boxes', 'URL'], trimOwner=False)
+
+                else:
+                        for (roll, version, release) in self.getRollNames(args, params):
+                                self.db.execute("""select r.arch, r.os from rolls r
+                                        where r.name='%s' and r.version='%s' and
+                                        r.rel='%s' """
+                                        % (roll, version, release))
+
+                                # For each pallet determine it is enabled
+                                # in any boxes.
+
+                                for arch, osname in self.db.fetchall():
+                                        self.db.execute("""select b.name from
+                                                stacks s, rolls r, boxes b where
+                                                r.name='%s' and r.arch='%s' and
+                                                r.version='%s' and r.rel='%s' and
+                                                s.roll=r.id and s.box=b.id """
+                                                % (roll, arch, version, release))
+
+                                        boxes = []
+                                        for box, in self.db.fetchall():
+                                                boxes.append(box)
+
+                                        self.addOutput(roll, (version, release, arch,
+                                                              osname, ' '.join(boxes)))
+
+                        self.endOutput(header=['name', 'version', 'release', 'arch',
+                                'os', 'boxes'], trimOwner=False)
+
+
+
+
+
+RollName = "stacki"
